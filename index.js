b6fa1a4e8418a2605770ca8e483ed20ed8beeaba
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-06-22 12:01:28 +0100

    Fix npm module export.

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-angular.module('textAngular-uploadImage', []).config(['$provide', function($provide) {
-  $provide.decorator('taOptions', ['taRegisterTool', 'taToolFunctions', function(taRegisterTool, taToolFunctions) {
-    taRegisterTool('uploadImage', {
-      iconclass: "fa fa-picture-o",
-      tooltiptext: 'Upload an image',
-      onElementSelect: {
-        element: 'img',
-        action: taToolFunctions.imgOnSelectAction
-      },
-      action: function() {
-        var $editor = this.$editor;
+angular.module('textAngular-uploadImage', [])
+  .config(['$provide', function($provide) {
+    $provide.decorator('taOptions', ['taRegisterTool', 'taToolFunctions', function(taRegisterTool, taToolFunctions) {
+      taRegisterTool('uploadImage', {
+        iconclass: "fa fa-picture-o",
+        tooltiptext: 'Upload an image',
+        onElementSelect: {
+          element: 'img',
+          action: taToolFunctions.imgOnSelectAction
+        },
+        action: function() {
+          var $editor = this.$editor;
 
-        // Create a virtual input element.
-        var input = document.createElement('input');
-        input.type = 'file';
-        input.accept = "image/*";
+          // Create a virtual input element.
+          var input = document.createElement('input');
+          input.type = 'file';
+          input.accept = "image/*";
 
-        input.onchange = function() {
-          var reader = new FileReader();
+          input.onchange = function() {
+            var reader = new FileReader();
 
-          if (this.files && this.files[0]) {
-            reader.onload = function(e) {
-              $editor().wrapSelection('insertHtml', '<img src=' + e.target.result + '>', true);
-            };
+            if (this.files && this.files[0]) {
+              reader.onload = function(e) {
+                $editor().wrapSelection('insertHtml', '<img src=' + e.target.result + '>', true);
+              };
 
-            reader.readAsDataURL(this.files[0]);
-          }
-        };
+              reader.readAsDataURL(this.files[0]);
+            }
+          };
 
-        // Click on a virtual input element.
-        input.click();
-      }
-    });
+          // Click on a virtual input element.
+          input.click();
+        }
+      });
+    }]);
   }]);
-}]);
+
+module.exports = 'textAngular-uploadImage';
